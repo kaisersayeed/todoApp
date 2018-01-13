d6da67d1ac3c8a1a185fcf3be752671602fedfc1
KAISER SAYEED <kaiser.sayeed07@gmail.com>
2018-01-13 18:59:38 +0600

    Handled todo completion.
    -- strike through todo if completed.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const Todo = ({todo, toggleTodo}) => (
-  <li onClick={(e)=>{
-    e.preventDefault();
-    toggleTodo(todo.id)
-  }}>
+  <li
+    style={{
+      textDecoration: todo.completed ? 'line-through' : 'none'
+    }}
+    onClick={(event)=> onTodoItemClick(event, todo.id, toggleTodo)}
+    >
     {todo.text}
   </li>
 );
+
+const onTodoItemClick = (event, id, toggleTodo) => {
+  event.preventDefault();
+  toggleTodo(id);
+};
 
 export default Todo;
