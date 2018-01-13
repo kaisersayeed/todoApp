bd26ab7d6adcdb16ecfcb968e3fcdf3f130906c2
KAISER SAYEED <kaiser.sayeed07@gmail.com>
2018-01-13 12:46:26 +0600

    Added root component.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kaisersayeed on 1/13/18.
+ */
+
+import React from 'react'
+
+const App = () => (
+    <div>
+      This is a simple Todo App.
+    </div>
+);
+
+export default App;
+
